a3e6782ef38253b6db1a74ae38149da312c90cc8
yeet_sneet <146510145+YeetSneet@users.noreply.github.com>
2024-12-21 03:25:38 -0500

    Update quadtree.lua

--- quadtree/quadtree.lua
+++ quadtree/quadtree.lua
@@ -7,9 +7,9 @@ function quadtree.create(initData, width, height, homogeneousFunc, maxcpu)
     
     local leafArray = {}
     local stack = {}
-    local stacklen = 1
+    local stacklen = 4
     
-    local remove, insert, floor = table.remove, table.insert, math.floor
+    
     local maxCpu = maxcpu or cpuMax()*0.2
     
     local obj = {
@@ -17,42 +17,45 @@ function quadtree.create(initData, width, height, homogeneousFunc, maxcpu)
         leafLen = leafLen
     }
     
-    insert(stack, height)
-    insert(stack, width)
-    insert(stack, 0)
-    insert(stack, 0)
+    stack[1] = height
+    stack[2] = width
+    stack[3] = 0
+    stack[4] = 0
     
+    local remove, floor, insert = table.remove, math.floor, table.insert
     function obj:think()
+        
         while cpuUsed() < maxCpu and stacklen>0 do
             
-            local x, y, w, h = remove(stack), remove(stack), remove(stack), remove(stack)
+            local x, y, w, h = stack[stacklen], stack[stacklen-1], stack[stacklen-2], stack[stacklen-3]
             
             if w == 1 and h == 1 then
-                stacklen = stacklen - 1
+                stacklen = stacklen - 4
                 self.leafLen = insert(leafArray, {x, y, w, h, initData[x + y * width + 1]})
             else
                 local isHomogeneous, value = homogeneousFunc(initData, x, y, w, h, width)
                 if isHomogeneous then 
-                    stacklen = stacklen - 1
+                    stacklen = stacklen - 4
                     self.leafLen = insert(leafArray, {x, y, w, h, value})
                 else
                     local half_w = floor(w / 2)
                     local half_h = floor(h / 2)
-                    
-                    stacklen = stacklen + 3
-                    -- nwNode
-                    insert(stack,half_h);insert(stack,half_w);insert(stack,y);insert(stack, x); 
-                    -- neNode
-                    insert(stack,half_h);insert(stack,w - half_w);insert(stack,y);insert(stack, x + half_w);
-                    -- swNode
-                    insert(stack,h - half_h);insert(stack,half_w);insert(stack,y + half_h);insert(stack, x);
-                    -- seNode
-                    insert(stack,h - half_h);insert(stack,w - half_w);insert(stack,y + half_h);insert(stack, x + half_w);
+            
+                    stacklen = stacklen+12
+                    stack[stacklen-15] = half_h;     stack[stacklen-14] = half_w;     stack[stacklen-13] = y;          stack[stacklen-12] = x;
+                    stack[stacklen-11] = half_h;     stack[stacklen-10] = w - half_w; stack[stacklen-9]  = y;          stack[stacklen-8]  = x + half_w;
+                    stack[stacklen-7]  = h - half_h; stack[stacklen-6]  = half_w;     stack[stacklen-5]  = y + half_h; stack[stacklen-4]  = x;
+                    stack[stacklen-3]  = h - half_h; stack[stacklen-2]  = w - half_w; stack[stacklen-1]  = y + half_h; stack[stacklen]    = x + half_w;
                 end
             end
         end
         
-        return stacklen<=0
+        if stacklen<=0 then
+            stack = nil
+            return true
+        else
+            return false
+        end 
     end
     return obj
 end
