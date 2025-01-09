00f795a5f728338028e0bef6463a09faf1c54a23
yeet_sneet <146510145+YeetSneet@users.noreply.github.com>
2025-01-08 21:45:50 -0500

    Update simple_example.lua

--- quadtree/simple_example.lua
+++ quadtree/simple_example.lua
@@ -1,43 +1,55 @@
 --@name quadtree example
 --@author yeet_sneet
---@shared
---@include quadtree.txt
+--@client
+--@include Quadtree.txt
 
-local quadtree = dofile("quadtree.txt")
+local quadtree = dofile("Quadtree.txt")
+
+local width, height = 10, 10
 
 local data = {
-    1, 1, 1, 1, 0, 0, 0, 0,
-    1, 1, 1, 1, 0, 0, 0, 0,
-    1, 1, 1, 1, 0, 0, 0, 0,
-    1, 1, 1, 1, 0, 0, 0, 0,
-    0, 0, 0, 0, 1, 1, 1, 1,
-    0, 0, 0, 0, 1, 1, 1, 1,
-    0, 0, 0, 0, 1, 1, 1, 1,
-    0, 0, 0, 0, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 
+    1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 
+    1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 
+    1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 
+    1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 
+    0, 0, 0, 0, 0, 1, 1, 1, 1, 1,    
+    0, 0, 0, 0, 0, 1, 1, 1, 1, 1,  
+    0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 
+    0, 0, 0, 0, 0, 1, 1, 1, 1, 1,  
+    0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 
 }
 
-local width, height = 8, 8
-local maxcpu = cpuMax()*0.2
 
-local QTree = quadtree.create(data, width, height, function (Data, sx, sy, w, h, totalWidth)
-    local ov = Data[sx + sy * totalWidth + 1]
+local function homogeneous(initData, startX, startY, width, height, totalWidth)
+    -- invalid sizes will happen when the width or height is not a power of 2
+    if width == 0 or height == 0 then return true, nil end 
     
-    for y = sy, sy + h - 1 do
-        local offset = y * totalWidth + 1
-        for x = sx, sx + w - 1 do
-            if Data[offset + x] ~= ov then
+    local value = initData[startX + startY * totalWidth + 1]
+    for y = startY, startY + height - 1 do
+        for x = startX, startX + width - 1 do
+            if initData[x + y * totalWidth + 1] ~= value then
                 return false
             end
         end
     end
     
-    return true, ov
-end, maxcpu)
+    return true, value
+end
+
+
+local QTree = quadtree.create(data, width, height, homogeneous, cpuMax()*0.2)
 
 hook.add("think", "create", function () 
     if QTree:think() then
-        print("done")
-        printTable(QTree)
+        QTree:finish()
+        
+        local data = QTree.leafArray
+        local unpack = quadtree.unpack
+        for i=1, QTree.leafLen do
+            print(unpack(data[i]))
+        end
+        
         hook.remove("think", "create")
     end
 end)
