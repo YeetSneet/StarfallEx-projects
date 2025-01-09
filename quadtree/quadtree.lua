21d3b544b2e049213a28055ab4ae95bf753f5a88
yeet_sneet <146510145+YeetSneet@users.noreply.github.com>
2025-01-08 21:28:40 -0500

    Update quadtree.lua

--- quadtree/quadtree.lua
+++ quadtree/quadtree.lua
@@ -3,59 +3,103 @@
 
 local quadtree = {}
 
+local floor = math.floor
+function quadtree.unpack(obj)
+    local x, w = obj[1], obj[2]
+    
+    local y = floor(x/33554432)
+    x = x % 33554432
+             
+    local h = floor(w/33554432)
+    w = w % 33554432
+    return x, y, w, h, obj[3]
+end
+
+function quadtree.pack(x, y, w, h, data)
+    return {x + y*33554432, w + h*33554432, data}
+end
+
+
 function quadtree.create(initData, width, height, homogeneousFunc, maxcpu)
-    
     local leafArray = {}
+    local leafLen = 0
     local stack = {}
-    local stacklen = 4
+    local stacklen = 2
+    local cpuTime = 0
     
+    maxcpu = maxcpu or cpuMax()*0.2
     
-    local maxCpu = maxcpu or cpuMax()*0.2
+    local obj = {}
     
-    local obj = {
-        leafArray = leafArray,
-        leafLen = 0
-    }
+    stack[1] = 0
+    stack[2] = width + height*33554432
     
-    stack[1] = height
-    stack[2] = width
-    stack[3] = 0
-    stack[4] = 0
-    
-    local remove, floor, insert = table.remove, math.floor, table.insert
+    local floor = math.floor
+
     function obj:think()
         
-        while cpuUsed() < maxCpu and stacklen>0 do
+        while cpuUsed() < maxcpu and stacklen>0 do
+            local x = stack[stacklen-1]
+            local y = floor(x/33554432)
+            x = x % 33554432
             
-            local x, y, w, h = stack[stacklen], stack[stacklen-1], stack[stacklen-2], stack[stacklen-3]
             
-            if w == 1 and h == 1 then
-                stack[stacklen]=nil;stack[stacklen-1]=nil;stack[stacklen-2]=nil;stack[stacklen-3]=nil;
-                stacklen = stacklen - 4
-                self.leafLen = insert(leafArray, {x, y, w, h, initData[x + y * width + 1]})
-            else
-                local isHomogeneous, value = homogeneousFunc(initData, x, y, w, h, width)
-                if isHomogeneous then 
-                    stack[stacklen]=nil;stack[stacklen-1]=nil;stack[stacklen-2]=nil;stack[stacklen-3]=nil;
-                    stacklen = stacklen - 4
-                    self.leafLen = insert(leafArray, {x, y, w, h, value})
-                else
-                    local half_w = floor(w / 2)
-                    local half_h = floor(h / 2)
+            local w = stack[stacklen]
+            local h = floor(w/33554432)
+            w = w % 33554432
             
-                    stacklen = stacklen+12
-                    stack[stacklen-15] = half_h;     stack[stacklen-14] = half_w;     stack[stacklen-13] = y;          stack[stacklen-12] = x;
-                    stack[stacklen-11] = half_h;     stack[stacklen-10] = w - half_w; stack[stacklen-9]  = y;          stack[stacklen-8]  = x + half_w;
-                    stack[stacklen-7]  = h - half_h; stack[stacklen-6]  = half_w;     stack[stacklen-5]  = y + half_h; stack[stacklen-4]  = x;
-                    stack[stacklen-3]  = h - half_h; stack[stacklen-2]  = w - half_w; stack[stacklen-1]  = y + half_h; stack[stacklen]    = x + half_w;
+            local isHomogeneous, value = homogeneousFunc(initData, x, y, w, h, width)
+            if isHomogeneous then 
+                stacklen = stacklen - 2
+                if value ~= nil then
+                    leafLen = leafLen + 1
+                    leafArray[leafLen] = {x+y*33554432, w+h*33554432, value}
                 end
+            else
+                local half_w = floor(w / 2)
+                local half_h = floor(h / 2)*33554432
+                h = h * 33554432
+                y = y * 33554432
+                
+                stacklen = stacklen+6
+                
+                stack[stacklen-7] = x + y;
+                stack[stacklen-6] = half_w + half_h;             
+                
+                stack[stacklen-5] = x + half_w + y; 
+                stack[stacklen-4] = (w - half_w) + half_h;       
+                
+                stack[stacklen-3] = x + (y + half_h);
+                stack[stacklen-2] = half_w + (h - half_h);      
+                
+                stack[stacklen-1] = (x + half_w) + (y + half_h);
+                stack[stacklen  ] = (w - half_w) + (h - half_h); 
             end
+        
         end
+        cpuTime = cpuTime + cpuUsed()
         
         return stacklen<=0
     end
+    
+    function obj:finish()
+        for k, _ in pairs(self) do
+            self[k] = nil
+        end
+        
+        self.leafLen = leafLen
+        self.cpuTime = cpuTime
+        self.leafArray = leafArray
+        
+        leafArray = nil
+        leafLen = nil
+        stack = nil
+        stacklen = nil
+        cpuTime = nil
+    end
+    
     return obj
 end
 
-return quadtree
 
+return quadtree
